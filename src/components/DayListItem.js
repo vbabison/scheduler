24ea2d1d53981f9f416b41f4c8ec3f767f06b82c
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-03-22 20:38:03 -0400

    Building the DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const formatSpots = spots => {
+    if (!spots) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return spots + " spots remaining";
+    }
+  };
+
+  return (
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
