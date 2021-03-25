0997943794d93f036be38299a6774c69ae77e12a
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-03-25 14:36:01 -0400

    adding appointment to cypress

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,9 +12,9 @@ function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`)
+      axios.get(`http://localhost:8001/api/days`),
+      axios.get(`http://localhost:8001/api/appointments`),
+      axios.get(`http://localhost:8001/api/interviewers`)
     ]).then(all => {
       dispatch({
         type: SET_APPLICATION_DATA,
